c3caa5ffe043189c121a900a09dbfda89c8ddbd9
jul-ian <julianaviles@gmail.com>
2023-07-10 22:02:09 -0700

    Adding code to create iris train-test ready for modeling

--- scripts/data/clean_iris.py
+++ scripts/data/clean_iris.py
@@ -12,3 +12,23 @@ iris_raw = pd.read_csv(irispath, names=names_iris)
 
 iris_raw.to_csv(join(datadir, 'processed/iris.csv'))
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MaxMinScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+num_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+cat_cols = ['species']
+
+num_pipeline = Pipeline([
+    ('scaler', MinMaxScaler())
+    ])
+cat_pipeline = Pipeline([
+    ('encoder', LabelEncoder())
+    ])
+
+full_pipeline = ColumnTransformer([
+    ('num', num_pipeline, num_cols),
+    ('cat', cat_pipeline, cat_cols)
+    ])
+iris_clean = full_pipeline.fit_transform()
