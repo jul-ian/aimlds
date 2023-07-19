4ce9d1fca7d244c3c4add158dcc7171cedfd0593
jul-ian <julianaviles@gmail.com>
2023-07-18 22:42:37 -0700

    Finished script to get chis data, will test.

--- scripts/data/get_chis_puf.py
+++ scripts/data/get_chis_puf.py
@@ -4,16 +4,23 @@ Get the PUF CHIS data
 
 import os
 import re
+from tempfile import mkdtemp
 from zipfile import ZipFile
 import pandas as pd
+data_dir = os.path.expanduser('~/Github/aimlds/data')
+raw_dir = os.path.join(data_dir, 'raw')
+proc_dir = os.path.join(data_dir, 'processed')
 
-data_dir = os.path.expanduser('~/Github/aimlds/data/raw')
-chis_zipfiles = [x for x in os.listdir(data_dir) if re.search(r'.+sas.*\.zip$', x)]
+chis_zipfiles = [x for x in os.listdir(raw_dir) if re.search(r'.+sas.*\.zip$', x)]
+chis_zipfiles = list(map(lambda s: os.path.join(raw_dir, s), chis_zipfiles))
 
-datadict = {}
+temp_dir = mkdtemp()
 for zipfile in chis_zipfiles:
-    with ZipFile(os.path.join(data_dir, zipfile)) as file:
+    with ZipFile(zipfile) as file:
         sasfile = [x for x in file.namelist() if x.endswith('sas7bdat')]
-        file.extract(sasfile[0], )
-        sasdf = pd.read_sas(sasfile[0])
-        datadict[re.sub(r'', r'', )]
+        file.extract(sasfile[0], temp_dir)
+        sasdf = pd.read_sas(os.path.join(temp_dir, sasfile[0]))
+        outfname = os.path.dirname(sasfile[0]) + '.pkl'
+        outfname = outfname.replace('_sas', '')
+        sasdf.to_pickle(os.path.join(proc_dir, outfname))
+        
