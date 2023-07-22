7b6513f4e2a2b5c43fcfdd2cb1bdbffcbbed2f9b
jul-ian <julianaviles@gmail.com>
2023-07-22 14:31:24 -0700

    Changed the encoding structure for the data to try to improve performance.

--- scripts/tensorflow/mlp_chis.py
+++ scripts/tensorflow/mlp_chis.py
@@ -6,7 +6,7 @@ Using tensorflow with CHIS data
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, LabelEncoder
 import tensorflow as tf
 from tensorflow.keras import layers
 
@@ -19,25 +19,26 @@ features = ['srsex', 'srage_p1', 'ahedc_p1', 'ombsrr_p1', 'marit2', 'ur_clrt6', 
 target = ['ab1']
 
 adult = adult_1920[features + target]
-adult = adult.assign(ab1 = lambda df: df[target[0]] - 1)
+#adult = adult.assign(ab1 = lambda df: df[target[0]] - 1)
 adult_train, adult_test = train_test_split(adult, test_size=0.2)
 
-encoder = OneHotEncoder(sparse=False)
-X_train = encoder.fit_transform(adult_train[features])
-X_test = encoder.transform(adult_test[features])
-y_train = adult_train[target].to_numpy(dtype='int64')
-y_test = adult_test[target].to_numpy(dtype='int64')
-
+one_hot_encoder = OneHotEncoder(sparse=False)
+ordinal_encoder = OrdinalEncoder()
+label_encoder = LabelEncoder()
+X_train = ordinal_encoder.fit_transform(adult_train[features])
+X_test = ordinal_encoder.transform(adult_test[features])
+y_train = label_encoder.fit_transform(adult_train[target])
+y_test = label_encoder.transform(adult_test[target])
 
 mlp_model = tf.keras.Sequential([
-    layers.Dense(512, activation='relu', input_shape=X_train.shape[1:]),
+    layers.Dense(1024, activation='relu', input_shape=X_train.shape[1:]),
     layers.Dense(256, activation='relu'),
     layers.Dense(64, activation='relu'),
     layers.Dense(64, activation='relu'),
     layers.Dense(5, activation='softmax')
     ])
 mlp_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-mlp_model.fit(X_train, y_train, epochs=30, validation_split=0.2)
+mlp_model.fit(X_train, y_train, epochs=10, validation_split=0.2)
 
 
 
