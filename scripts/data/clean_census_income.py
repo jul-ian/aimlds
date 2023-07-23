1a4e4d9573b1eee018d73898a0b510804ddaadfd
jul-ian <julianaviles@gmail.com>
2023-07-23 16:57:47 -0700

    Cleaning the census income data.

--- scripts/data/clean_census_income.py
+++ scripts/data/clean_census_income.py
@@ -13,3 +13,31 @@ col_names = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marita
 income_raw = pd.read_csv(income_path, names=col_names)
 
 income_raw.to_csv('~/Github/aimlds/data/processed/census_income.csv')
+
+## feature engineering
+workclass_map = {
+    'private': ['Private'],
+    'government': ['Local-gove', 'State-gov', 'Federal-gov'],
+    'self_employed': ['Self-emp-not-inc', 'Self-emp-inc'],
+    'other': ['?', 'Without-pay', 'Never-worked']
+    }
+education_map = {
+    'less_than_hs': ['10th', '11th','12th','1st-4th', 
+                     '5th-6th', '7th-8th', '9th', 'Preschool'],
+    'hs': ['HS-grad', 'Some-college'],
+    'college': ['Assoc-acdm', 'Assoc-voc', 'Bachelors', 'Prof-school'],
+    'advance_college': ['Masters', 'Doctorate']
+    }
+marital_map = {
+    'married': ['Married-civ-spouse', 'Married-spouse-absent', 'Married-AF-spouse'],
+    'never_married': ['Never-married'],
+    'no_longer_married': ['Divorced', 'Separated', 'Widowed']
+    }
+relationship_map = {
+    'husband': ['Husband'], 'wife': ['Wife'],
+    'other': ['Not-in-family', 'Own-child', 'Unmarried', 'Other-relative']
+    }
+
+race_map = {
+    
+    }
